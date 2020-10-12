64364618df93098dc3f392d53a6afc2b667721e2
Greysf <72762042+Greysf@users.noreply.github.com>
2020-10-12 15:45:56 -0300

    Create estructuras.js

---
+++ estructuras.js
@@ -0,0 +1,13 @@
+var nombre = 'Greys';
+var apellido = 'ferreira';
+var sueldo = 12000
+var edad = 26
+var esCasado = false;
+
+if ( nombre == "") {
+    alert("Usted es un anonimo");
+} else if ( sueldo < 10000 ); {
+    alert ('su sueldo es bajo');
+}  ( esCasado == false ); {
+    alert ('ud es soletero');
+}
