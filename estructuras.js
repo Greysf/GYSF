f64d24c2b6e44674a42f1e139a4b9f758b1e1732
Greysf <72762042+Greysf@users.noreply.github.com>
2020-10-12 18:29:13 -0300

    Add files via upload

--- estructuras.js
+++ estructuras.js
@@ -1,3 +1,6 @@
+var enunciado = document.getElementById("tit");
+alert(enunciado.innerHTML)
+
 var nombre = 'Greys';
 var apellido = 'ferreira';
 var sueldo = 12000
@@ -9,5 +12,37 @@ if ( nombre == "") {
 } else if ( sueldo < 10000 ); {
     alert ('su sueldo es bajo');
 }  ( esCasado == false ); {
-    alert ('ud es soletero');
+    alert ('ud es soltero');
 }
+
+
+var categ= "1a";
+
+switch (categ){
+    case "1a":
+        sueldo = 100
+            break;
+
+    case "1b":
+        sueldo = 200
+            break;
+
+    case "1c":
+        sueldo = 300
+            break;
+    
+    case "1d":
+        sueldo = 400
+            break;
+
+    case "1e":
+        sueldo = 500
+            break;
+
+}
+console.log (categ)
+
+/* new date (year, month, day, hours, minutes, seconds, miliseconds);
+new date (miliseconds);
+new date (date string); */
+
